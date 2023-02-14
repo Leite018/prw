2c8ccf8d59fd4df8b8e78e7688f4213b60950249
Leite018 <matheushenriiqueazevedoleite@gmail.com>
2023-02-14 11:02:19 -0300

    Meu primeiro programa em php

---
+++ aula1/ola2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!--= exemplo de codigo HTML-->
+    <h1>Exemplo de Olá Mundo</h1>
+
+    <?php
+    //exemplo de codigo PHP
+    echo "<h2>Olá IFSP</h2>";
+    ?>
+</body>
+</html>
